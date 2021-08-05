9ae4dc9e6910722f78a3591ece039de405f52089
hoon <devkwonsehoon@naver.com>
2021-08-05 21:29:53 +0900

    update : router

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { RankModule } from './rank/rank.module';
 import { UserModule } from './user/user.module';
 import { BattleModule } from './battle/battle.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [UserModule, RankModule, BattleModule],
+  imports: [UserModule, RankModule, BattleModule, AuthModule],
   controllers: [AppController],
   providers: [],
 })
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -12,19 +12,19 @@ import { SignupDto } from 'src/common/dto/signup.dto';
 import { UpdateDataDto } from 'src/common/dto/updateData.dto';
 
 @ApiTags('User')
-@Controller('api/users')
+@Controller('api/user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
-  // users
-  @Get()
+  // api/users
+  @Get('')
   @ApiOkResponse({ description: '유저 데이터 조회 성공' })
   @ApiUnauthorizedResponse({ description: 'Invalid Credential' })
   userInfo() {
     return 'get user info';
   }
 
-  // users/:id
+  // api/users/:id
   @Get('/:id')
   @ApiOkResponse({ description: '특정 유저 데이터 조회 성공' })
   @ApiUnauthorizedResponse({ description: 'Invalid Credential' })
@@ -32,30 +32,12 @@ export class UserController {
     return 'get partial user info';
   }
 
-  // users/:id
+  // api/users/:id
   @Patch('/:id')
   @ApiOkResponse({ description: '유저 정보 수정 성공' })
   @ApiUnauthorizedResponse({ description: 'Invalid Credential' })
   @ApiBody({ type: UpdateDataDto })
   nickname(@Param('id') userId: number, @Body() updateData) {
     return 'edit user info';
-  }
-
-  // users/login
-  @Post('/login')
-  @ApiOkResponse({ description: '로그인 성공' })
-  @ApiUnauthorizedResponse({ description: 'Invalid Credential' })
-  @ApiBody({ type: LoginDto })
-  login(): string {
-    return this.userService.login();
-  }
-
-  // users/signup
-  @Post('/signup')
-  @ApiOkResponse({ description: '회원가입 성공' })
-  @ApiUnauthorizedResponse({ description: 'Invalid Credential' })
-  @ApiBody({ type: SignupDto })
-  signup(): string {
-    return this.userService.signup();
   }
 }
