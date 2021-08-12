2ab92cdb93995065990e60dea0d614b60973fd9a
hoon <devkwonsehoon@naver.com>
2021-08-12 20:41:45 +0900

    add: title router

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -49,28 +49,4 @@ export class UserController {
   editSchool(@Param('id') userId: number, @Body() updateData) {
     return 'edit user school';
   }
-
-  // /users/:id/title
-  @Get('/:id/title')
-  @ApiOperation({
-    summary: '유저 타이틀 히스토리 조회',
-    description: '유저 타이틀 히스토리 조회를 진행합니다.',
-  })
-  @ApiOkResponse({ description: '유저 타이틀 히스토리 조회 성공' })
-  @ApiUnauthorizedResponse({ description: 'Invalid Credential' })
-  getTitleHistory(@Param('id') userId: number) {
-    return 'get user title history';
-  }
-
-  // /users/:id/title
-  @Patch('/:id/title')
-  @ApiOperation({
-    summary: '유저 타이틀 수정',
-    description: '유저 타이틀 수정을 진행합니다.',
-  })
-  @ApiOkResponse({ description: '유저 타이틀 수정 성공' })
-  @ApiUnauthorizedResponse({ description: 'Invalid Credential' })
-  editTitle(@Param('id') userId: number, @Body() updateData) {
-    return 'edit user title';
-  }
 }
