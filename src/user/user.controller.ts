efc2186b1dfb29c3703c623845deb52bb93a3688
hoon <devkwonsehoon@naver.com>
2021-08-12 13:34:47 +0900

    update : router

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -38,6 +38,7 @@ export class UserController {
     return 'get partial user info';
   }
 
+<<<<<<< Updated upstream
   // /users/:id/school
   @Patch('/:id/school')
   @ApiOperation({
@@ -45,6 +46,15 @@ export class UserController {
     description: '유저 학교 수정을 진행합니다.',
   })
   @ApiOkResponse({ description: '유저 소속 학교 수정 성공' })
+=======
+  // api/users/:id
+  @Patch('/:id')
+  @ApiOperation({
+    summary: '유저 데이터 수정',
+    description: '유저 데이터 수정을 진행합니다.',
+  })
+  @ApiOkResponse({ description: '유저 정보 수정 성공' })
+>>>>>>> Stashed changes
   @ApiUnauthorizedResponse({ description: 'Invalid Credential' })
   editSchool(@Param('id') userId: number, @Body() updateData) {
     return 'edit user school';
