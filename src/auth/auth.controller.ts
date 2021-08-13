5e0beb64cc88814a630d41d914c3c9874f5ddd92
hoon <devkwonsehoon@naver.com>
2021-08-13 15:58:52 +0900

    add : google oauth

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -7,6 +7,8 @@ import {
   Patch,
   Post,
   Put,
+  Req,
+  UseGuards,
 } from '@nestjs/common';
 import {
   ApiBody,
@@ -19,22 +21,62 @@ import {
 import { AuthService } from './auth.service';
 import { LoginDto } from 'src/common/dto/login.dto';
 import { SignupDto } from 'src/common/dto/signup.dto';
+import { AuthGuard } from '@nestjs/passport';
+import { get } from 'https';
 
 @ApiTags('Auth')
 @Controller('/auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
-  // /auth/login
-  @Post('/login')
+  // /auth/google
+  @Get('/google')
   @ApiOperation({
-    summary: '로그인',
-    description: '로그인을 진행합니다.',
+    summary: '구글 로그인 시도',
+    description: '구글 로그인을 시도합니다.',
   })
-  @ApiOkResponse({ description: '로그인 성공' })
+  @ApiOkResponse({ description: '구글 로그인 시도 성공' })
+  @ApiUnauthorizedResponse({ description: 'Invalid Credential' })
+  @UseGuards(AuthGuard('google'))
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
+  async googleAuth(@Req() req) {}
+
+  // /auth/google
+  @Get('/google/redirect')
+  @ApiOperation({
+    summary: '구글 로그인',
+    description: '구글 로그인을 진행합니다.',
+  })
+  @ApiOkResponse({ description: '구글 로그인 성공' })
+  @ApiUnauthorizedResponse({ description: 'Invalid Credential' })
+  @UseGuards(AuthGuard('google'))
+  googleAuthRedirect(@Req() req) {
+    return this.authService.googleLogin(req);
+  }
+
+  // /auth/apple
+  @Get('/apple')
+  @ApiOperation({
+    summary: '애플 로그인',
+    description: '애플 로그인을 진행합니다.',
+  })
+  @ApiOkResponse({ description: '애플 로그인 성공' })
   @ApiUnauthorizedResponse({ description: 'Invalid Credential' })
   @ApiBody({ type: LoginDto })
-  login(): string {
+  appleAuth(): string {
+    return 'login';
+  }
+
+  // /auth/apple
+  @Get('/apple/redirect')
+  @ApiOperation({
+    summary: '애플 로그인',
+    description: '애플 로그인을 진행합니다.',
+  })
+  @ApiOkResponse({ description: '애플 로그인 성공' })
+  @ApiUnauthorizedResponse({ description: 'Invalid Credential' })
+  @ApiBody({ type: LoginDto })
+  appleAuthRedirect(): string {
     return 'login';
   }
 
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,15 @@
 import { Injectable } from '@nestjs/common';
-
+import { User } from '.prisma/client';
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  googleLogin(req) {
+    if (!req.user) {
+      return '유저가 존재하지 않습니다.';
+    }
+
+    return {
+      message: '구글 로그인 성공',
+      user: req.user,
+    };
+  }
+}
