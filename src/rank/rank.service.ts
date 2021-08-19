c1d760fa9e2cebab4ccc846dc6417ad555ac0831
whywhyy <kangsan5240@naver.com>
2021-08-19 13:45:18 +0900

    feat : ranks/schools/:id 개인랭킹 구현

--- src/rank/rank.service.ts
+++ src/rank/rank.service.ts
@@ -1,8 +1,47 @@
 import { Injectable } from '@nestjs/common';
-
+import { Profile } from 'src/user/user.types';
+import { PrismaService } from '../prisma/prisma.service';
+import { UserService } from '../user/user.service';
 @Injectable()
 export class RankService {
-  getRank(): string {
-    return 'will get rank';
+  constructor(
+    private prisma: PrismaService,
+    private userService: UserService,
+  ) {}
+
+  async getRankByScoolId(schoolId: number): Promise<Profile[]> {
+    const users = await this.prisma.user.findMany({
+      where: {
+        schoolId: schoolId,
+      },
+    });
+    const lolAccountIdToProfileMap: Map<string, Profile> = new Map();
+    for (const user of users) {
+      // todo => getProfileByUserIds
+      const profile = await this.userService.getProfileByUserId(user.id);
+      lolAccountIdToProfileMap.set(user.LOLAccountId, profile);
+    }
+    const lolAccountIds = users.map((user) => user.LOLAccountId);
+    const category = await this.prisma.lOLSummaryElement.findFirst({
+      where: {
+        LOLMatchFieldName: '티어',
+      },
+    });
+
+    //
+    const ranks = await this.prisma.lOLSummaryPersonal.findMany({
+      where: {
+        AND: { LOLSummaryElementId: category.id },
+        LOLAccountId: { in: lolAccountIds },
+      },
+      orderBy: {
+        value: 'desc',
+      },
+    });
+    const results = [];
+    for (const rank of ranks) {
+      results.push(lolAccountIdToProfileMap.get(rank.LOLAccountId));
+    }
+    return results;
   }
 }
---
+++ test/rank/rank.test.ts
@@ -0,0 +1,102 @@
+import { initSchema } from '../commn/schemaUtil';
+import { RankModule } from '../../src/rank//rank.module';
+import { RankService } from '../../src/rank/rank.service';
+import { UserService } from '../../src/user/user.service';
+import * as request from 'supertest';
+import { Test } from '@nestjs/testing';
+import { INestApplication } from '@nestjs/common';
+import { PrismaService } from '../../src/prisma/prisma.service';
+import { UserModule } from '../../src/user/user.module';
+import { PrismaModule } from '../../src/prisma/prisma.module';
+//
+import { LOLService } from '../../src/lol/lol.service';
+import { LOLModule } from '../../src/lol/lol.module';
+//
+import { AuthService } from '../../src/auth/auth.service';
+import { AuthModule } from '../../src/auth/auth.module';
+
+describe('simple etst', () => {
+  let app: INestApplication;
+
+  const prismaService = new PrismaService();
+  const userService = new UserService(prismaService);
+  const rankService = new RankService(prismaService, userService);
+  beforeAll(async () => {
+    const moduleRefAuth = await Test.createTestingModule({
+      imports: [RankModule, UserModule, PrismaModule, LOLModule, AuthModule],
+      providers: [
+        UserService,
+        RankService,
+        PrismaService,
+        LOLService,
+        AuthService,
+      ],
+    })
+      .overrideProvider(RankService)
+      .useValue(rankService)
+      .compile();
+
+    app = moduleRefAuth.createNestApplication();
+    await app.init();
+  });
+
+  afterAll(() => {
+    app.close();
+    prismaService.$disconnect();
+  });
+
+  beforeEach(async () => {
+    await initSchema(prismaService);
+  });
+
+  it('school rank test', async () => {
+    // setup
+    // school - foo
+    await prismaService.school.create({
+      data: {
+        name: 'foo',
+        division: 'foo',
+        region: 'foo',
+        address: 'foo',
+      },
+    });
+    // setup Title
+    await prismaService.lOLSummaryElement.create({
+      data: {
+        LOLMatchFieldName: '티어',
+        calculateType: 'foo',
+        sortType: 'desc',
+        exposureName: 'foo',
+      },
+    });
+    const resSignUp = await request(app.getHttpServer())
+      .post('/auth/signup')
+      .set('Accept', 'application/json')
+      .type('application/json')
+      .send({ email: 'abc@abc.com', LOLNickName: 'kkangsan', schoolId: 1 });
+
+    expect(resSignUp.statusCode).toBe(201);
+    const { id, email, LOLAccountId, schoolId } = resSignUp.body;
+    expect(id).toBe(1);
+    expect(email).toBe('abc@abc.com');
+    expect(LOLAccountId).toBeTruthy();
+    expect(schoolId).toBe(1);
+    expect(1).toBe(1);
+    //e2e;
+    const resRankSchool = await request(app.getHttpServer())
+      .get(encodeURI('/ranks/schools/1'))
+      .set('Accept', 'application/json')
+      .type('application/json');
+
+    const { lol } = resRankSchool.body[0];
+    expect(resRankSchool.body[0].id).toBeTruthy();
+    const { profileIconId, profileIconImageUrl, summonerLevel, tierInfo } = lol;
+    expect(profileIconId).toBeTruthy();
+    expect(profileIconImageUrl).toBeTruthy();
+    expect(summonerLevel).toBeTruthy();
+    const { tier, rank, leaguePoints } = tierInfo;
+    expect(tier).toBeTruthy();
+    expect(rank).toBeTruthy();
+    expect(leaguePoints >= 0).toBeTruthy();
+  });
+});
