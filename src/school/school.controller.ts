0bd6e37e1775332d9fed52cb0bb57e07a9adc267
whywhyy <kangsan5240@naver.com>
2021-08-25 13:43:23 +0900

    fix : 학교 검색 수정, division 쿼리 추가

--- src/school/school.controller.ts
+++ src/school/school.controller.ts
@@ -7,25 +7,31 @@ import {
   Patch,
   Post,
   Put,
+  Query,
 } from '@nestjs/common';
 import {
   ApiBody,
   ApiOkResponse,
   ApiOperation,
   ApiProperty,
+  ApiQuery,
   ApiResponse,
   ApiTags,
   ApiUnauthorizedResponse,
 } from '@nestjs/swagger';
 import { SchoolService } from './school.service';
 import { School } from './school.entity';
+import { Division } from './school.model';
+
 @ApiTags('School')
 @Controller('/schools')
 export class SchoolController {
   constructor(private readonly schoolService: SchoolService) {}
 
-  // /schools/search/:searchWord
-  @Get('/search/:searchWord')
+  // /schools/search?searchWord&division
+  @Get('/search')
+  @ApiQuery({ name: 'searchWord' })
+  @ApiQuery({ name: 'division', required: false, enum: Division })
   @ApiOperation({
     summary: '학교 검색',
     description: '학교 이름 조회를 진행합니다. (최대 50개)',
@@ -36,8 +42,14 @@ export class SchoolController {
     isArray: true,
   })
   @ApiUnauthorizedResponse({ description: 'Invalid Credential' })
-  async getSchoolListBySearchParam(@Param('searchWord') searchWord: string) {
-    return await this.schoolService.getSchoolListBySearchParam(searchWord);
+  async getSchoolListBySearchParam(
+    @Query('searchWord') searchWord: string,
+    @Query('division') division?: string,
+  ) {
+    return await this.schoolService.getSchoolListBySearchParamOptionalDivision({
+      searchWord: searchWord,
+      division: division,
+    });
   }
 
   // /schools
--- test/school/school.test.ts
+++ test/school/school.test.ts
@@ -38,10 +38,25 @@ describe('school data test', () => {
 
   it('search schools ', async () => {
     const res = await request(app.getHttpServer())
-      .get(encodeURI('/schools/search/서울'))
+      .get(encodeURI('/schools/search?searchWord=서울'))
+      .set('Accept', 'application/json')
+      .type('application/json');
+    // console.log(res.body);
+    expect(res.body.length).toBeGreaterThan(0);
+    expect(res.body[0].id).toBeTruthy();
+    expect(res.body[0].name).toBeTruthy();
+    expect(res.body[0].division).toBeTruthy();
+    expect(res.body[0].regionId).toBeTruthy();
+    expect(res.body[0].address).toBeTruthy();
+  });
+
+  it('search schools with division ', async () => {
+    const res = await request(app.getHttpServer())
+      .get(encodeURI('/schools/search?searchWord=선학&division=초등학교'))
       .set('Accept', 'application/json')
       .type('application/json');
 
+    expect(res.body.length).toBe(2);
     expect(res.body[0].id).toBeTruthy();
     expect(res.body[0].name).toBeTruthy();
     expect(res.body[0].division).toBeTruthy();
