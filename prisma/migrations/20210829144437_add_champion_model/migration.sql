af4270394666bfeac2230c8a94ef5a2078cf0ceb
whywhyy <kangsan5240@naver.com>
2021-08-29 23:44:42 +0900

    fix : champion mastery table fix champion model

--- prisma/migrations/20210829144437_add_champion_model/migration.sql
+++ prisma/migrations/20210829144437_add_champion_model/migration.sql
@@ -1,14 +1,20 @@
 /*
   Warnings:
 
+  - You are about to drop the column `championId` on the `LOLChampionMastery` table. All the data in the column will be lost.
   - The primary key for the `LOLSummaryPersonal` table will be changed. If it partially fails, the table could be left without primary key constraint.
   - You are about to drop the column `championId` on the `LOLSummaryPersonal` table. All the data in the column will be lost.
   - The primary key for the `LOLSummarySchool` table will be changed. If it partially fails, the table could be left without primary key constraint.
   - You are about to drop the column `championId` on the `LOLSummarySchool` table. All the data in the column will be lost.
   - A unique constraint covering the columns `[LOLAccountId,LOLSummaryElementId,LOLChampionId]` on the table `LOLSummaryPersonal` will be added. If there are existing duplicate values, this will fail.
   - A unique constraint covering the columns `[SchoolId,LOLSummaryElementId,LOLChampionId]` on the table `LOLSummarySchool` will be added. If there are existing duplicate values, this will fail.
+  - Added the required column `LOLChampionId` to the `LOLChampionMastery` table without a default value. This is not possible if the table is not empty.
 
 */
+-- AlterTable
+ALTER TABLE "LOLChampionMastery" DROP COLUMN "championId",
+ADD COLUMN     "LOLChampionId" TEXT NOT NULL;
+
 -- AlterTable
 ALTER TABLE "LOLSummaryPersonal" DROP CONSTRAINT "LOLSummaryPersonal_pkey",
 DROP COLUMN "championId",
@@ -51,3 +57,6 @@ ALTER TABLE "LOLSummaryPersonal" ADD FOREIGN KEY ("LOLChampionId") REFERENCES "L
 
 -- AddForeignKey
 ALTER TABLE "LOLSummarySchool" ADD FOREIGN KEY ("LOLChampionId") REFERENCES "LOLChampion"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "LOLChampionMastery" ADD FOREIGN KEY ("LOLChampionId") REFERENCES "LOLChampion"("id") ON DELETE CASCADE ON UPDATE CASCADE;
