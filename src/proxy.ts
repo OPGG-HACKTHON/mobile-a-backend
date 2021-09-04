e615a2346814d801e55a22f83d1d89b9cc85e925
hoon <devkwonsehoon@naver.com>
2021-09-04 12:53:04 +0900

    add: proxy 작업중

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,19 +1,27 @@
 // TODO: local test (작업중)
 
-import proxy from 'redbird';
+// import proxy from 'redbird';
 
-proxy({
-  port: 80,
-  ssl: {
-    http2: true,
-    port: 443, // SSL port used to serve registered https routes with LetsEncrypt certificate.
-    key: './secrets/AuthKey_ZC25JZUJ4G.key',
-    cert: './secrets/certificate.crt',
-  },
-});
+// proxy({
+//   port: 80,
+//   ssl: {
+//     http2: true,
+//     port: 443, // SSL port used to serve registered https routes with LetsEncrypt certificate.
+//     key: './secrets/AuthKey_ZC25JZUJ4G.key',
+//     cert: './secrets/certificate.crt',
+//   },
+// });
 
-// Angular apps
+// // Angular apps
 
-proxy.register('api.opggmobilea.com', 'http://localhost:9999');
-// NestJS services
-proxy.register('api.opggmobilea.com/auth/apple', 'http://localhost:3000');
+// proxy.register('api.opggmobilea.com', 'http://localhost:9999', {
+//   port: 80,
+//   ssl: {
+//     http2: true,
+//     port: 443, // SSL port used to serve registered https routes with LetsEncrypt certificate.
+//     key: './secrets/AuthKey_ZC25JZUJ4G.key',
+//     cert: './secrets/certificate.crt',
+//   },
+// });
+// // NestJS services
+// proxy.register('api.opggmobilea.com/auth/apple', 'http://localhost:3000');
